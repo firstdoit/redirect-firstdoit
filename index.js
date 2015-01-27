6706323375ceba8ffadec4b35bf2a29de2d2125d
Guilherme Rodrigues <gadr90@gmail.com>
2015-01-27 10:42:49 -0200

    Fix #1

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ var server = http.createServer(function(req, res) {
         res.end()
     } else {
         res.statusCode = 301;
-        res.setHeader("Location", "http://blog.firstdoit.com"+parsedURL.pathname+"?utm_source=gadrme");
-        console.log("301 - " + parsedURL.pathname);
+        var hostname = "firstdoit.com";
+        if (req.headers.host.indexOf('blog') === 0) {
+            hostname = "blog.firstdoit.com";
+        }
+        var location = "http://" + hostname + parsedURL.pathname + "?utm_source=gadrme";
+        res.setHeader("Location", location);
+        console.log("301 - " + location);
         res.end();
     }
 });
 
-server.listen(process.env.PORT || 8081);
+server.listen(process.env.PORT || 8083);
